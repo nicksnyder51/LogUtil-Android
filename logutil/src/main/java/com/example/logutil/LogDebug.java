dea34fbb59be7a84f601fa02ae978aeeb668774d
nsnyder <nick.snyder@4cite.com>
2017-07-14 13:04:54 -0400

    - Added set methods for new members: UserDeviceID and EmailAddress - added displaySharedPrefs method

--- logutil/src/main/java/com/example/logutil/LogDebug.java
+++ logutil/src/main/java/com/example/logutil/LogDebug.java
@@ -1,6 +1,7 @@
 package com.example.logutil;
 
 import android.app.Activity;
+import android.content.Context;
 import android.widget.Toast;
 
 /**
@@ -8,15 +9,22 @@ import android.widget.Toast;
  */
 
 public class LogDebug {
-    private static String UID;
+    private static String UserDeviceID;
     private static String CID;
+    private static String EmailAddress;
 
-    public static void setUID(String uid) {
-        UID = uid;
+    public static void setUserDeviceID(String u_d_id) {
+        UserDeviceID = u_d_id;
     }
-    public static void setCID(String cid, Activity a) {
+    public static void setCID(String cid) {
         CID = cid;
-        Toast.makeText(a, "Cid - " + CID,
+    }
+    public static void setEmailAddress(String emailAddress) {
+        EmailAddress = emailAddress.trim().toLowerCase();
+    }
+    public static void displaySharedPrefs(Context ctx) {
+        Toast.makeText(ctx, "Cid - " + CID + "\nUser Device ID - " + UserDeviceID + "\nEmail Address - " + EmailAddress,
                 Toast.LENGTH_LONG).show();
     }
+
 }
