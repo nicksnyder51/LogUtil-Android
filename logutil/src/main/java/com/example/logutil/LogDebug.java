9d27a5da436460f654c272a9c440c5a447bab802
nsnyder <nick.snyder@4cite.com>
2017-07-19 12:27:12 -0400

    - Added code to make RESTful api calls

--- logutil/src/main/java/com/example/logutil/LogDebug.java
+++ logutil/src/main/java/com/example/logutil/LogDebug.java
@@ -3,7 +3,8 @@ package com.example.logutil;
 import android.app.Activity;
 import android.content.Context;
 import android.util.DisplayMetrics;
-import android.widget.Toast;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 /**
  * Created by nsnyder on 7/13/2017 for 4Cite Marketing.
@@ -12,11 +13,13 @@ import android.widget.Toast;
 public class LogDebug {
     private static String UserDeviceID;
     private static String CID;
-    private static String EVENT_DTTM;
-    private static int DISPLAY_WIDTH;
-    private static int DISPLAY_HEIGHT;
-    private static String EVENT_TITLE;
     private static String EmailAddress;
+    private static String EVENT_DTTM;
+    private static String DISPLAY_WIDTH;
+    private static String DISPLAY_HEIGHT;
+    private static String EVENT_TITLE;
+    private static String TZ_OFFSET;
+    private static String CONTEXT;
 
 
     public static void setCID(String cid) {
@@ -24,6 +27,9 @@ public class LogDebug {
     }
     public static void setUserDeviceID(String u_d_id) {
         UserDeviceID = u_d_id;
+    }
+    public static void setEmailAddress(String emailAddress) {
+        EmailAddress = emailAddress.trim().toLowerCase();
     }
     public static void setEventDttm(String EventDttm) {
         EVENT_DTTM = EventDttm;
@@ -33,22 +39,40 @@ public class LogDebug {
         ((Activity) ctx).getWindowManager()
                 .getDefaultDisplay()
                 .getMetrics(displayMetrics);
-        DISPLAY_WIDTH = displayMetrics.widthPixels;
-        DISPLAY_HEIGHT = displayMetrics.heightPixels;
+        DISPLAY_WIDTH = String.valueOf(displayMetrics.widthPixels);
+        DISPLAY_HEIGHT = String.valueOf(displayMetrics.heightPixels);
     }
     public static void setEventTitle(String eventTitle) {
         EVENT_TITLE = eventTitle;
     }
-    public static void setEmailAddress(String emailAddress) {
-        EmailAddress = emailAddress.trim().toLowerCase();
+    public static void setTZOffset(int tzOffset) {
+        TZ_OFFSET = String.valueOf(tzOffset);
     }
     public static void displaySharedPrefs(Context ctx) {
-        Toast.makeText(ctx, "Cid - " + CID + "\nUser Device ID - " + UserDeviceID + "\nEmail Address - " + EmailAddress,
-                Toast.LENGTH_LONG).show();
-        Toast.makeText(ctx, "Display size WxH - " + DISPLAY_WIDTH + "x" + DISPLAY_HEIGHT + "\nEvent title - " + EVENT_TITLE +
-                        "\nEvent datetime - " + EVENT_DTTM,
-                Toast.LENGTH_LONG).show();
+        RequestParams rp = new RequestParams();
+        rp.add("sv_cid ", CID); rp.add("sv_session", UserDeviceID); rp.add("sv_svem", EmailAddress);
+        rp.add("sv_dt", EVENT_DTTM); rp.add("sv_width", DISPLAY_WIDTH); rp.add("sv_height ", DISPLAY_HEIGHT);
+        rp.add("sv_title", EVENT_TITLE); rp.add("sv_tzOffset ", TZ_OFFSET); rp.add("sv_ver", "1.6.6"); rp.add("sv_context", CONTEXT);
+
+        HttpUtils.post("https://track.securedvisit.com", rp, new JsonHttpResponseHandler() {
+            /*@Override
+            public void onSuccess(int statusCode, PreferenceActivity.Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                Log.d("asd", "---------------- this is response : " + response);
+                try {
+                    JSONObject serverResp = new JSONObject(response.toString());
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onSuccess(int statusCode, PreferenceActivity.Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+
+            }*/
+        });
 
     }
-
 }
