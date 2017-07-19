49c917d86b0aa7d0ed0cb2365091b7d5be8d2af6
nsnyder <nick.snyder@4cite.com>
2017-07-19 15:03:02 -0400

    - Changed code to return response

--- logutil/src/main/java/com/example/logutil/LogDebug.java
+++ logutil/src/main/java/com/example/logutil/LogDebug.java
@@ -28,7 +28,7 @@ public class LogDebug {
     private static String EVENT_TITLE;
     private static String TZ_OFFSET;
     private static String CONTEXT = "";
-    private static JSONObject serverResp;
+    private static int STATUSCODE;
 
 
     public static void setCID(String cid) {
@@ -57,7 +57,7 @@ public class LogDebug {
     public static void setTZOffset(int tzOffset) {
         TZ_OFFSET = String.valueOf(tzOffset);
     }
-    public static JSONObject displaySharedPrefs(Context ctx) {
+    public static int displaySharedPrefs(Context ctx) {
         RequestParams rp = new RequestParams();
         rp.add("sv_cid ", CID); rp.add("sv_session", UserDeviceID); rp.add("sv_svem", EmailAddress);
         rp.add("sv_dt", EVENT_DTTM); rp.add("sv_width", DISPLAY_WIDTH); rp.add("sv_height ", DISPLAY_HEIGHT);
@@ -66,9 +66,10 @@ public class LogDebug {
         HttpUtils.post("https://track.securedvisit.com", rp, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                STATUSCODE = statusCode;
                 // If the response is JSONObject instead of expected JSONArray
                 try {
-                    serverResp = new JSONObject(response.toString());
+                    JSONObject serverResp = new JSONObject(response.toString());
                 } catch (JSONException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -78,19 +79,22 @@ public class LogDebug {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
                 // Pull out the first event on the public timeline
-
+                STATUSCODE = statusCode;
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                try {
-                    serverResp = new JSONObject(errorResponse.toString());
-                } catch (JSONException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
+                STATUSCODE = statusCode;
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                STATUSCODE = statusCode;
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                STATUSCODE = statusCode;
             }
         });
-        return(serverResp);
+        return(STATUSCODE);
     }
 }
