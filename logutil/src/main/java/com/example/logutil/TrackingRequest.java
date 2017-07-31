309df01b2d275f74484007fe161c2406518de972
nsnyder <nick.snyder@4cite.com>
2017-07-31 15:30:38 -0400

    - Changes have been made

--- logutil/src/main/java/com/example/logutil/TrackingRequest.java
+++ logutil/src/main/java/com/example/logutil/TrackingRequest.java
@@ -17,7 +17,7 @@ public class TrackingRequest extends StringRequest{
     private static final String REGISTER_REQUEST_URL = "https://track.securedvisit.com";
     private Map<String, String> params;
 
-    public TrackingRequest(String cid, String session, String svem, String dt, String width, String height, String title, String offset,  String context, Response.Listener<String> listener){
+    public TrackingRequest(String cid, String session, String svem, String dt, String width, String height, String title, String offset, String context, Response.Listener<String> listener){
         super(Method.GET, REGISTER_REQUEST_URL, listener, null);
         params = new HashMap<>();
         params.put("sv_cid", cid);
@@ -39,8 +39,8 @@ public class TrackingRequest extends StringRequest{
         return super.parseNetworkResponse(response);
     }
 
-    //@Override
-    //public Map<String, String> getParams() {
-    //    return params;
-    //}
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
 }
